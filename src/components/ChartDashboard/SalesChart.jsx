d4ac76ac3373756b0f23a54276a1d3b83f6ec481
lima <joaopaulo.gomeslima8@gmail.com>
2021-12-31 14:28:35 -0300

    Ajusta posicionamento dos eixos no grafico.

--- src/components/ChartDashboard/SalesChart.jsx
+++ src/components/ChartDashboard/SalesChart.jsx
@@ -2,28 +2,46 @@ import React from 'react';
 import { Line } from 'react-chartjs-2';
 
 function SalesChart() {
+
+  const options = {
+    scales: {
+      y: {
+        display: true,
+        position: 'right',
+        grid: {
+          drawOnChartArea: false,
+        }
+      }
+    }
+  };
+
   const data = {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+    
+    // o ponto no grafico com o valor vai ser o dia
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'], // Aqui vai ser todos os meses
     datasets: [
       {
-        label: 'First dataset',
+        label: 'Este mês',
         data: [33, 53, 85, 41, 44, 65],
         fill: true,
-        backgroundColor: 'rgba(75,192,192,0.2)',
-        borderColor: 'rgba(75,192,192,1)',
+        lineTension: 0.5,
+        backgroundColor: 'rgba(252,60,141,0.1)',
+        borderColor: 'rgba(252,60,141)',
+        yAxisID: 'y',
       },
       {
-        label: 'Second dataset',
+        label: 'Mês passado',
         data: [33, 25, 35, 51, 54, 76],
         fill: false,
+        lineTension: 0.5,
         borderColor: '#742774',
       },
     ],
   };
 
   return (
-    <div className=''>
-      <Line data={data} />
+    <div className='chart'>
+      <Line options={options} data={data} />
     </div>
   );
 }
